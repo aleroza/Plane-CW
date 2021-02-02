8a70ef18d5439b7685ca48703a7b2b9e6938da28
Your Name <you@example.com>
2021-02-02 17:56:29 +0300

    blah-blah new errors added

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ class Node:
     def __init__(self, lb, tb):
         self.val = None
         self.mb = None
-        self.lb = lb  # "next" left branch
-        self.tb = tb  # "next" top branch
+        self.lb = lb  # prev left branch
+        self.tb = tb  # prev top branch
 
     def calculate_mb(self):
         self.mb = math.sqrt(self.tb[0] ** 2 + self.lb[0] ** 2)
 
 
-def algorithm(nodes, x, y, v):
-    if nodes[0][0].val:
-        pass
+def algorithm(cur_node, v):
+    cur_node.val=v
+    print(cur_node.lb[0])
+    if cur_node.lb != None:
+        algorithm(cur_node.lb[0], cur_node[1]+v)
+        
 
-    return nodes
-
-
-def gen_rawdata(x, y):
+def gen_rawdata(y, x):
     rawdata = []
     for row in range(y):
         cur_row = []
@@ -38,30 +38,37 @@ def gen_nodes(rawdata):
             if x == 0:
                 lb = None
             else:
-                lb = [col[0], cur_row[x - 1]]
+                lb = [cur_row[x - 1], col[0]]
             if y == 0:
                 tb = None
             else:
-                tb = [col[1], nodes[y - 1][x]]
+                tb = [nodes[y - 1][x], col[1]]
             cur_row.append(Node(lb, tb))
         nodes.append(cur_row)
     return nodes
 
 
 def main():
-    # nodes = gen_nodelist(gen_graph(6,5))
-    # nodes[0][0].val=0
-    # nodes = algorithm(nodes)
+    x,y=6,5;
 
-    g = gen_rawdata(6, 5)
-    for row in g:
-        print(row)
-    print("")
-    nl = gen_nodes(g)
-    for row in nl:
-        for col in row:
-            print(f"[{col.lb} {col.tb}]", end="")
-        print("")
+    nodes = gen_nodes(gen_rawdata(y,x))
+    print(len(nodes),len(nodes[0]))  
+    nodes[y-1][x-1].val=0
+    nodes = algorithm(nodes[y-1][x-1],0)
+    
+    
+  
+    # for row in nodes:
+        # for col in row:
+            # print(f"[{col.lb} {col.tb}]", end="")
+        # print("")
+        
+    #g = gen_rawdata(6, 5)
+    #for row in g:
+    #    print(row)
+    #print("")
+    #nl = gen_nodes(g)
+
 
 
 if __name__ == '__main__':
