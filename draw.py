019aa635a57931d34e6f524177f74ef0b1caf087
aleroza <aleroza1910@gmail.com>
2021-02-06 19:05:03 +0300

    new shitty bugs

--- draw.py
+++ draw.py
@@ -1,40 +1,64 @@
+import json
+import random
 import matplotlib.pyplot as plt
 import networkx as nx
+import main
+import numpy as np
+from itertools import count
+
+# for x in range(x):
+#   for y in range(y):
+#
+
+# G.add_node("n00", weight=9)
+# G.add_node("n01", weight=3)
+# G.add_node("n10", weight=4)
+# G.add_node("n11", weight=0)
+
+# G.add_edge("n11", "n01", weight=3)
+# G.add_edge("n11", "n10", weight=4)
+# G.add_edge("n01", "n00", weight=6)
+# G.add_edge("n10", "n00", weight=7)
+
+def draw():
+	x, y = 7, 6
+	with open('example_data.json', 'r') as file:
+		w = np.array(json.load(file))
+	nodes = main.gen_nodes(w)
+	G = nx.grid_2d_graph(x, y)
+	pos = dict((n, n) for n in G.nodes())
+	for yy, nodes_row in enumerate(nodes):
+		for xx, node in enumerate(nodes_row):
+			if node.lb is not None:
+				G[xx, yy][xx - 1, yy]['weight'] = node.lb[1]
+			if node.db is not None:
+				G[xx, yy][xx, yy - 1]['weight'] = node.db[1]
+	route = []
+	colors=[]
+	for node in G.nodes:
+		if node in [(0,0),(x-1,y-1)]:
+			colors.append("red")
+		elif node in route:
+			colors.append("green")
+		else:
+			colors.append("gray")
+
+	weights = nx.get_edge_attributes(G, "weight")
+	labels = dict(((i, j), f"{i},{j}") for i, j in G.nodes())
+	nx.draw_networkx(G, pos, with_labels=True, labels=labels, node_size=500, node_color=colors)
+	nx.draw_networkx_edge_labels(G, pos, edge_labels=weights)
+
+	plt.axis('off')
+	plt.show()
 
 
-def main():
-  G = nx.grid_2d_graph(7,6)
-  # G.add_node("n00", weight=9)
-  # G.add_node("n01", weight=3)
-  # G.add_node("n10", weight=4)
-  # G.add_node("n11", weight=0)
-
-  # G.add_edge("n11", "n01", weight=3)
-  # G.add_edge("n11", "n10", weight=4)
-  # G.add_edge("n01", "n00", weight=6)
-  # G.add_edge("n10", "n00", weight=7)
-
-  pos = dict( (n, n) for n in G.nodes() )
-  labels = dict(((i, j), i + (1-1-j) * 1) for i, j in G.nodes())
-  print(pos)
-  print("\n")
-  print(labels)
-  weights = nx.get_edge_attributes(G, "weight")
-  ebunch=["(0,0)","(0,1)"]
-  G.remove_edges_from(ebunch)
-  nx.draw_networkx(G, pos, with_labels=True, labels=labels)
-  nx.draw_networkx_edge_labels(G, pos, edge_labels=weights)
-  
-  plt.axis('off')
-  plt.show()
-
-  # x,y=7,6
-  # for row in range(y):
-  #       cur_row = []
-  #       for col in range(x):
-  #           cur_row.append([random.randint(1, 10), random.randint(1, 10)])
-  #       rawdata.append(cur_row)
+# x,y=7,6
+# for row in range(y):
+#       cur_row = []
+#       for col in range(x):
+#           cur_row.append([random.randint(1, 10), random.randint(1, 10)])
+#       rawdata.append(cur_row)
 
 
 if __name__ == '__main__':
-    main()
+	draw()
--- main.py
+++ main.py
@@ -1,94 +1,92 @@
 import math
-import random
 import json
+import numpy as np
 
 
 class Node:
-    def __init__(self, lb, tb):
-        self.val = None
-        self.mb = None
-        self.lb = lb  # prev left branch
-        self.tb = tb  # prev top branch
-        self.opt = None
+	def __init__(self, lb, db):
+		self.val = None
+		self.mb = None
+		self.lb = lb  # prev left branch
+		self.db = db  # prev down branch
+		self.opt = []
 
-    def calculate_mb(self):
-        self.mb = math.sqrt(self.tb[0] ** 2 + self.lb[0] ** 2)
+	def calculate_mb(self):
+		self.mb = math.sqrt(self.db[0] ** 2 + self.lb[0] ** 2)
 
 
 def algorithm(cur_node, v, prev_node):
-    if cur_node.val == None or cur_node.val>v:
-        cur_node.val=v
-        cur_node.opt=prev_node
-    elif cur_node.val<v:
-        return
+	if cur_node.val is None or cur_node.val > v:
+		cur_node.val = v
+		cur_node.opt = [prev_node]
+	elif cur_node.val == v:
+		cur_node.opt.append(prev_node)
+	elif cur_node.val < v:
+		return
 
-    if cur_node.lb != None:
-        algorithm(cur_node.lb[0], cur_node.lb[1]+v, cur_node)
-    if cur_node.tb != None:
-        algorithm(cur_node.tb[0], cur_node.tb[1]+v, cur_node)
-    
+	if cur_node.lb is not None:
+		algorithm(cur_node.lb[0], cur_node.lb[1] + v, cur_node)
+	if cur_node.db is not None:
+		algorithm(cur_node.db[0], cur_node.db[1] + v, cur_node)
 
 
 def gen_rawdata(y, x):
-    rawdata = []
-    for row in range(y):
-        cur_row = []
-        for col in range(x):
-            cur_row.append([random.randint(1, 10), random.randint(1, 10)])
-        rawdata.append(cur_row)
-    return rawdata
+	return np.random.randint(1, 10, (y, x, 2), int)
 
 
 def gen_nodes(rawdata):
-    nodes = []
-    for y, row in enumerate(rawdata):
-        cur_row = []
-        for x, col in enumerate(row):
-            if x == 0:
-                lb = None
-            else:
-                lb = [cur_row[x - 1], col[0]]
-            if y == 0:
-                tb = None
-            else:
-                tb = [nodes[y - 1][x], col[1]]
-            cur_row.append(Node(lb, tb))
-        nodes.append(cur_row)
-    return nodes
+	nodes = []
+	for y, row in enumerate(rawdata):
+		cur_row = []
+		for x, col in enumerate(row):
+			if x == 0:
+				lb = None
+			else:
+				lb = [cur_row[x - 1], col[0]]
+			if y == 0:
+				db = None
+			else:
+				db = [nodes[y - 1][x], col[1]]
+			cur_row.append(Node(lb, db))
+		nodes.append(cur_row)
+	return nodes
+
 
 def print_nodes(nodes):
-    for row in nodes:
-        for col in row:
-            print(f"[{col.val}]", end="")
-        print("")
-    print("\n")
+	for row in nodes[::-1]:
+		for col in row:
+			print(f"[{col.val}]", end="")
+		print("")
+	print("\n")
 
-def optimal_path(cur_node):
-    if cur_node.opt == None:
-        return
-    print(cur_node.val)
-    optimal_path(cur_node.opt)
+
+def optimal_path(cur_node, opt_indexes, nodes):
+	print(cur_node)
+	if cur_node is None:
+		return opt_indexes
+	for y, path in enumerate(cur_node.opt):
+		[opt_indexes.append((noderow.index(path), y)) for noderow in nodes if path in noderow]
+		optimal_path(path, opt_indexes, nodes)
+
 
 def main():
-    x,y=7,6;
+	x, y = 5, 4
+	rawdata = gen_rawdata(y, x)
 
-    rawdata=gen_rawdata(y,x)
+	with open('example_data.json', 'r') as file:
+		rawdata = json.load(file)
+	nodes = gen_nodes(rawdata)
+	nodes[y - 1][x - 1].val = 0
 
-    with open('example_data.json', 'r') as file:
-        rawdata=json.load(file)
+	print_nodes(nodes)
 
-    nodes = gen_nodes(rawdata)
-    nodes[y-1][x-1].val=0
+	algorithm(nodes[y - 1][x - 1], 0, None)
 
-    print_nodes(nodes)
+	print_nodes(nodes)
 
-    algorithm(nodes[y-1][x-1],0, None)
-
-    print_nodes(nodes)
-
-    optimal_path(nodes[0][0])
-
+	opt_indexes = []
+	optimal_path(nodes[0][0], opt_indexes, nodes)
 
 
 if __name__ == '__main__':
-    main()
+	main()
